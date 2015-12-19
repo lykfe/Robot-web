d8300e8170f11151b2c804402c2d38e42a56622f
John Lim <johnlim0521@gmail.com>
2015-12-18 17:35:58 -0800

    added popover and scrollTo

---
+++ js/project.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('a[href^="#"]').click(function(e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $(target).animatescroll();
+	});
+
+	$("#popover").popover();
+});
